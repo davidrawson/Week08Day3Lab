191480dbb79b5ca1ab5793494ff82c7a414e35e3
David Rawson <dm.rawson@gmail.com>
2018-01-24 12:24:12 +0000

    View started.

--- app/src/main/java/example/codeclan/com/rockpaperscissors/GameActivity.java
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/GameActivity.java
@@ -2,12 +2,19 @@ package example.codeclan.com.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView moveText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        moveText = findViewById(R.id.move_text);
+
+
     }
 }
