c6ebc528058124ad44ab45d416b07ef6eb53451c
David Rawson <dm.rawson@gmail.com>
2018-01-24 14:13:26 +0000

    Scissors is working.

--- app/src/main/java/example/codeclan/com/rockpaperscissors/GameActivity.java
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/GameActivity.java
@@ -2,19 +2,51 @@ package example.codeclan.com.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
     TextView moveText;
+    View rockButton;
+    View paperButton;
+    View scissorsButton;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        moveText = findViewById(R.id.move_text);
+        moveText = findViewById(R.id.computer_move);
 
 
+    }
+
+    public void onRockButtonClicked(View button){
+        Move move = new Move();
+        move.setUpMoves();
+        Log.d(getClass().toString(), "Computers move: " + move.getRandomMove());
+        Game game = new Game(move.getRandomMove(), "rock");
+    }
+
+
+    public void onPaperButtonClicked(View button){
+        Move move = new Move();
+        move.setUpMoves();
+        Log.d(getClass().toString(), "Computers move: " + move.getRandomMove());
+        Game game = new Game(move.getRandomMove(), "paper");
+    }
+
+
+    public void onScissorsButtonClicked(View button){
+        Move move = new Move();
+        move.setUpMoves();
+        Log.d(getClass().toString(), "Computers move: " + move.getRandomMove());
+        Game game = new Game(move.getRandomMove(), "scissors");
+        String gameResult = game.play();
+        TextView resultView = findViewById(R.id.result);
+        resultView.setText(gameResult.toString());
     }
 }
